673b490e84188c874954a1f0a43de4eaf37847ea
Ahmar khan <ahmarbilal223344@gmail.com>
2022-09-14 23:30:16 -0700

    new changes

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   validateUser(userName);
-  // validateEmail(userEmail);
-  // validatePass(userPass, userRePass);
+  validateEmail(userEmail);
+  validatePass(userPass, userRePass);
 });
 
 // validate userName
@@ -25,35 +25,55 @@ function validateUser(userName) {
     massage[0].innerHTML = "invalid username";
     massage[0].classList.add("error");
     userName.classList.add("error-input");
-    // console.log("invalid user")
     return;
   }
   massage[0].innerHTML = "valide username";
   massage[0].classList.add("success");
   userName.classList.add("success-input");
-  userData.innerHTML = `Name ${userName.value} <br>`;
+  userData.innerHTML = `Name :${userName.value}`;
 }
 
 // validate email
 function validateEmail(userEmail) {
-  return String(userEmail)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+  if (
+    String(userEmail)
+      .toLowerCase()
+      .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  ) {
+    massage[1].innerHTML = "valid email";
+    massage[1].classList.add("success");
+    userEmail.classList.add("success-input");
+    userData.innerHTML = `Email :${userEmail.value}`;
+  } else {
+    massage[1].innerHTML = "invalid email";
+    massage[1].classList.add("error");
+    userEmail.classList.add("error-input");
+  }
 }
 
 // validate password
 
 function validatePass(userPass, userRePass) {
-  if (userPass.value.trim().length > 7 && userPass.value.trim().length < 20) {
+  if (userPass.value.trim().length > 5) {
+    massage[2].classList.add("success");
+    massage[2].innerHTML = "vlaid password";
+    userPass.classList.add("success-input");
+    userData.innerHTML = `Password :${userPass.value}`;
     if (userPass.value === userRePass.value) {
-      console.log(`Password : ${userPass.value}`);
+      massage[3].classList.add("success");
+      massage[3].innerHTML = "valide password";
+      userRePass.classList.add("success-input");
+      userData.innerHTML = `Confirm pass :${userRePass.value}`;
     } else {
-      alert(" Password donot match");
+      massage[3].classList.add("error");
+      massage[3].innerHTML = "Confirm password donot match";
+      userRePass.classList.add("error-input");
+      userEmail.classList.add("error-input");
     }
   } else {
-    alert("invalid password.Password should be atleast 8 characters long");
+    massage[2].classList.add("error");
+    massage[2].innerHTML = "invalid password";
+    userPass.classList.add("error-input");
   }
 }
 
@@ -63,5 +83,25 @@ userName.addEventListener("focusout", (e) => {
     userName.classList.add("success-input");
   } else {
     userName.classList.add("error-input");
+  }
+});
+userEmail.addEventListener("focusout", (e) => {
+  if (
+    String(userEmail)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      )
+  ) {
+    userEmail.classList.add("success-input");
+  } else {
+    userEmail.classList.add("error-input");
+  }
+});
+userPass.addEventListener("focusout", (e) => {
+  if (userPass.value.trim().length > 5) {
+    userPass.classList.add("success-input");
+  } else {
+    userPass.classList.add("error-input");
   }
 });
